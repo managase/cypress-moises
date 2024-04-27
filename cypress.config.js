fca77ba041b88b676c2f4c05ff37663e10274263
managase <nagase@gmail.com>
2024-04-26 22:54:56 -0300

    Ajust config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
+  requestTimeout: 3000,
+  responseTimeout: 20000,
+  retries: {
+    runMode: 2,
+    openMode: 2,
+  },
   e2e: {
+    baseUrl: 'https://studio.moises.ai',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@bahmutov/cy-grep/src/plugin')(config);
+      return config;
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+const registerCypressGrep = require('@bahmutov/cy-grep')
+registerCypressGrep()
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
