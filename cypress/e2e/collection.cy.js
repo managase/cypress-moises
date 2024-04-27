aacadea5576f54c82b4cc03599346aba16d446f6
managase <nagase@gmail.com>
2024-04-27 11:46:14 -0300

    Include Collection Test

---
+++ cypress/e2e/collection.cy.js
@@ -0,0 +1,33 @@
+// cypress/e2e/signup.cy.js
+
+const LoginPage = require('../support/page_objects/loginPageMoises');
+const Components = require('../support/page_objects/componentsMoises');
+
+describe('Collection Test', () => {
+
+    beforeEach(() => {
+        const username = Cypress.env('username')
+        const password = Cypress.env('password')
+        LoginPage.visit();
+        LoginPage.login(username, password);
+        Components.clickMoisesLogo();
+    });
+
+    afterEach(() => {
+        Components.clickUserInfoBtn();
+        Components.clickUserInfoSignoutBtn();
+        cy.get('#login_button').should('be.visible')
+    });
+
+    it('should hide the specific moises collection', { tags: '@e2e' }, () => {
+        cy.get('p.info_title__KHXaB').should('contain', 'Moises Collection')
+        Components.clickPlaylistActionBtn_1();
+        Components.clickExitSetlist();
+        Components.clickDeleteCollection();
+        // Click the button in case presented in the screen
+        cy.get('.modal-keyboard-shortcuts_close__kkQQ8').should('be.visible').click();
+        cy.get('#modal_button_close').should('be.visible').click();
+        Components.clickMoisesLogo();
+        cy.get('p.info_title__KHXaB').should('not.exist');
+    });
+});
