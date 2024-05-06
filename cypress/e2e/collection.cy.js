85f0757ecc09826691ee822407f32351055dbe0c
managase <nagase@gmail.com>
2024-05-06 15:26:07 -0300

    Test ajust

--- cypress/e2e/collection.cy.js
+++ cypress/e2e/collection.cy.js
@@ -1,33 +1,42 @@
 // cypress/e2e/collection.cy.js
+import { faker } from '@faker-js/faker';
 
+const SignupPage = require('../support/page_objects/signupPage');
 const LoginPage = require('../support/page_objects/loginPage');
 const Components = require('../support/page_objects/components');
 
 describe('Collection Test', () => {
 
     beforeEach(() => {
-        const username = Cypress.env('username')
-        const password = Cypress.env('password')
-        LoginPage.visit();
-        LoginPage.login(username, password);
-        Components.clickMoisesLogo();
+        const username = faker.internet.userName() + '@qatest.com';
+        const password = faker.internet.password();
+
+        cy.clearCookies();
+        cy.clearLocalStorage()
+        cy.reload();
+        SignupPage.visit();
+        SignupPage.clickSignupBtn();
+        SignupPage.signup(username, password);
+        Components.validateUserInfoBtn();
+        Components.clickUserInfoBtn();
     });
 
     afterEach(() => {
         Components.clickUserInfoBtn();
         Components.clickUserInfoSignoutBtn();
-        cy.get('#login_button').should('be.visible')
+        LoginPage.validateLoginBtn();
     });
 
     it('should hide the specific moises collection', { tags: '@e2e' }, () => {
-        cy.get('p.info_title__KHXaB').should('contain', 'Moises Collection')
+        Components.validateSetlistContent('Moises Collection');
         Components.clickPlaylistActionBtn_1();
         Components.clickExitSetlist();
         Components.clickDeleteCollection();
+
         // Click the button in case presented in the screen
-        cy.get('.modal-keyboard-shortcuts_close__kkQQ8').should('be.visible').click();
-        cy.get('#modal_button_close').should('be.visible').click();
+        Components.clickModalShortcutsClose();
+        Components.clickModalCloseBtn();
         Components.clickMoisesLogo();
-        cy.get('p.info_title__KHXaB').should('not.exist');
+        Components.validateDeleteCollection();
     });
 });
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -3,6 +3,7 @@
 import { faker } from '@faker-js/faker';
 
 const SignupPage = require('../support/page_objects/signupPage');
+const LoginPage = require('../support/page_objects/loginPage');
 const Components = require('../support/page_objects/components');
 const username = faker.internet.userName() + '@qatest.com';
 const password = faker.internet.password();
@@ -10,22 +11,17 @@ const password = faker.internet.password();
 describe('Signup Test', () => {
 
     beforeEach(() => {
+        cy.clearCookies();
+        cy.clearLocalStorage()
+        cy.reload();
         SignupPage.visit();
-    });
-
-    after(() => {
-        cy.writeFile('cypress.env.json', { 
-            "username": username,
-            "password": password
-          });
-    });
+      });
 
     it('should present a error message to enter a valid email address', () => {
         SignupPage.clickSignupBtn();
         cy.fixture('signupDataNegative.json').each((loginData) => {
             SignupPage.signup(loginData.username, loginData.password);
-                cy.get('.email-password-sign-in_labelError___Bgrm').as('labelError').should('be.visible')
-                cy.get('@labelError').contains(loginData.errorMessage);
+            SignupPage.validateLoginLabelError(loginData.errorMessage);    
             SignupPage.clearFields();
         })
     });
@@ -33,9 +29,10 @@ describe('Signup Test', () => {
     it('should signup with valid credentials', { tags: '@e2e' },  () => {
         SignupPage.clickSignupBtn();
         SignupPage.signup(username, password);
-        cy.get('#user_info_button').should('be.visible')
+        Components.validateUserInfoBtn();
+        SignupPage.saveSignupUserFile(username, password);
         Components.clickUserInfoBtn();
         Components.clickUserInfoSignoutBtn();
-        cy.get('#login_button').should('be.visible')
+        LoginPage.validateLoginBtn();
     });
 });
